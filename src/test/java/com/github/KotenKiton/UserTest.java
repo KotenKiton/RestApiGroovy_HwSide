fca5a25f251ef2fd3581fa335a689df2dad9b0e6
Koten <Kotenkiton@gmail.com>
2022-09-07 23:54:11 +0300

    User test working

--- src/test/java/com/github/KotenKiton/UserTest.java
+++ src/test/java/com/github/KotenKiton/UserTest.java
@@ -1,4 +1,15 @@
 package com.github.KotenKiton;
 
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
+
+    @Test
+    void singleUser(){
+        Specs.request
+                .when()
+                .get("/users/2")
+                .then()
+                .log().body();
+    }
 }
