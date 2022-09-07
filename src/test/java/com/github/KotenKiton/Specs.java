e756c247e295e6f5bbb0734c528602538e067242
Koten <Kotenkiton@gmail.com>
2022-09-07 23:28:44 +0300

    Added : 1) BaseUri 2) BasePath 3) log().all 4) contentType - Json

--- src/test/java/com/github/KotenKiton/Specs.java
+++ src/test/java/com/github/KotenKiton/Specs.java
@@ -1,10 +1,16 @@
 package com.github.KotenKiton;
 
+
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.with;
 
 public class Specs {
+    public static RequestSpecification request = with()
+            .baseUri("https://reqres.in/")
+            .basePath("/api")
+            .log().all()
+            .contentType(ContentType.JSON);
 
-    public static RequestSpecification request = with().baseUri("https://reqres.in/");
 }
