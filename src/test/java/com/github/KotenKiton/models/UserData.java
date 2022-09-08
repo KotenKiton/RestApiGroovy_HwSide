e085c5272f708619a5cb8084c826d5403c91fcbf
Koten <Kotenkiton@gmail.com>
2022-09-08 23:50:23 +0300

    Model added

--- src/test/java/com/github/KotenKiton/models/UserData.java
+++ src/test/java/com/github/KotenKiton/models/UserData.java
@@ -1,7 +1,13 @@
 package com.github.KotenKiton.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserData {
-    //private User data;
+    private User data;
 
+    public  User getData() {return data;}
 
+    public void setData(User data) {this.data = data;
+    }
 }
