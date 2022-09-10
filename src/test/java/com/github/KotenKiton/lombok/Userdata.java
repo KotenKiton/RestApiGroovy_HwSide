d1d08aa6e5dad949ad55ef8d8d65b89f627000a1
Koten <Kotenkiton@gmail.com>
2022-09-10 23:20:21 +0300

    46:42

--- src/test/java/com/github/KotenKiton/lombok/Userdata.java
+++ src/test/java/com/github/KotenKiton/lombok/Userdata.java
@@ -1,4 +1,13 @@
 package com.github.KotenKiton.lombok;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Userdata {
+    @JsonProperty("data")
+    private User user;
+
 }
