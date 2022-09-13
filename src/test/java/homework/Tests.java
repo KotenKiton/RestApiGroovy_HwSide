039c85e5ae88f8ac0ef4ef46ae1b67df3ff728e7
Koten <Kotenkiton@gmail.com>
2022-09-13 12:46:30 +0300

    Second test in progress

--- src/test/java/homework/Tests.java
+++ src/test/java/homework/Tests.java
@@ -1,6 +1,7 @@
 package homework;
 
 import homework.lombokModels.RequestLogin;
+import homework.lombokModels.RequestMorpheusJobIdCreatedAt;
 import homework.lombokModels.ResponseLogin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+import static org.hamcrest.Matchers.is;
 import static specs.Specs.request;
 import static specs.Specs.response;
 
@@ -18,7 +20,7 @@ public class Tests {
     void postStatusCode200() {
 
         Integer exceptId = 4;
-        String expectToken ="QpwL5tke4Pnpja7X4";
+        String expectToken = "QpwL5tke4Pnpja7X4";
 
         RequestLogin body = new RequestLogin();
         body.setEmail("eve.holt@reqres.in");
@@ -31,9 +33,29 @@ public class Tests {
                 .then().spec(response)
                 .extract().as(ResponseLogin.class);
 
-        Assertions.assertEquals(exceptId,responseLogin.getId());
-        Assertions.assertEquals(expectToken,responseLogin.getToken());
+        Assertions.assertEquals(exceptId, responseLogin.getId());
+        Assertions.assertEquals(expectToken, responseLogin.getToken());
+    }
 
+    @Test
+    @DisplayName("201test")
+    void postStatusCode201() {
+        RequestMorpheusJobIdCreatedAt body = new RequestMorpheusJobIdCreatedAt();
+        body.setName("Morpheus");
+        body.setJob("leader");
+        body.setId(484);
+        body.setCreatedAt("2022-09-13T09:16:11.144Z");
+
+
+        given()
+                .spec(request).body(body)
+                .when()
+                .post("/register")
+                .then().spec(response)
+                .log().all()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
     }
 
 
---
+++ src/test/java/homework/lombokModels/RequestMorpheusJobIdCreatedAt.java
@@ -0,0 +1,11 @@
+package homework.lombokModels;
+
+import lombok.Data;
+
+@Data
+public class RequestMorpheusJobIdCreatedAt{
+	private String createdAt;
+	private String name;
+	private Integer id;
+	private String job;
+}
